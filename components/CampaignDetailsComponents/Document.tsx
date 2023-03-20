17bf1e8e9d07f6386ec92cd54315a1c711e6da33
elijahthis <emperorelijah007@gmail.com>
2023-03-20 10:08:55 +0100

    docs

--- components/CampaignDetailsComponents/Document.tsx
+++ components/CampaignDetailsComponents/Document.tsx
@@ -1,21 +1,22 @@
 import { DocIcon } from "../svgs";
 import { BiArrowToBottom } from "react-icons/bi";
+import styles from "./CampaignDetailsComponents.module.scss";
 
 const Document = () => {
 	return (
-		<section>
+		<section className={styles.Document}>
 			<h4>Supporting Campaign Documents</h4>
-			<div>
+			<div className={styles.Docs}>
 				{Array(5)
 					.fill(0)
 					.map((item, ind) => (
-						<div key={ind}>
+						<div key={ind} className={styles.DocRow}>
 							<DocIcon />
 							<p>Transcripts.doc</p>
-							<div>
-								<BiArrowToBottom />
+							<button>
+								<BiArrowToBottom size={18} />
 								Download
-							</div>
+							</button>
 						</div>
 					))}
 			</div>
