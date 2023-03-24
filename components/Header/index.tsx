8e2650436d891f72410a5c0af0224b588cbd766a
elijahthis <emperorelijah007@gmail.com>
2023-03-24 09:49:24 +0100

    mobile bottom

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Logo from "../Logo";
-import { BellIcon } from "../svgs";
+import { BellIcon, CampaignIcon, OverviewIcon, PledgesIcon } from "../svgs";
 import styles from "./Header.module.scss";
 import ProfileImage from "../ProfileImage";
 import { TbChevronDown } from "react-icons/tb";
@@ -13,50 +13,68 @@ const Header = () => {
 	console.log("router", router);
 
 	const headerLinks = [
-		{ title: "Overview", link: "/" },
-		{ title: "All Campaigns", link: "/campaigns" },
-		{ title: "Pledges", link: "/pledges" },
+		{ title: "Overview", link: "/", icon: <OverviewIcon /> },
+		{ title: "All Campaigns", link: "/campaigns", icon: <CampaignIcon /> },
+		{ title: "Pledges", link: "/pledges", icon: <PledgesIcon /> },
 	];
 
 	return (
-		<header className={styles.Header}>
-			<Logo />
-			<div className={styles.Header__right}>
-				<ul className={styles.headerLinks}>
-					{headerLinks.map((linkItem, ind) => (
-						<Link
-							href={linkItem.link}
-							className={`${styles.linkItem} ${
-								router.pathname === linkItem.link
-									? styles["linkItem--active"]
-									: ""
-							}`}
-							key={ind}
+		<>
+			<header className={styles.Header}>
+				<Logo />
+				<div className={styles.Header__right}>
+					<ul className={styles.headerLinks}>
+						{headerLinks.map((linkItem, ind) => (
+							<Link
+								href={linkItem.link}
+								className={`${styles.linkItem} ${
+									router.pathname === linkItem.link
+										? styles["linkItem--active"]
+										: ""
+								}`}
+								key={ind}
+							>
+								<li>{linkItem.title}</li>
+							</Link>
+						))}
+					</ul>
+					<div className={styles.Header__right__side}>
+						<BellIcon />
+						<div className={styles.line}></div>
+						<div
+							className={styles.profileBit}
+							onClick={() => router.push("/profile/view")}
 						>
-							<li>{linkItem.title}</li>
-						</Link>
-					))}
-				</ul>
-				<div className={styles.Header__right__side}>
-					<BellIcon />
-					<div className={styles.line}></div>
-					<div
-						className={styles.profileBit}
-						onClick={() => router.push("/profile/view")}
-					>
-						<ProfileImage
-							url="/profileImg.jpeg"
-							alt="profile"
-							width={32}
-							height={32}
-							style={{ margin: "auto 0" }}
-						/>
-						<p>Philip</p>
-						<TbChevronDown size={16} />
+							<ProfileImage
+								url="/profileImg.jpeg"
+								alt="profile"
+								width={32}
+								height={32}
+								style={{ margin: "auto 0" }}
+							/>
+							<p>Philip</p>
+							<TbChevronDown size={16} />
+						</div>
 					</div>
 				</div>
+			</header>
+			<div className={styles.bottomHeader}>
+				{headerLinks.map((linkItem, ind) => (
+					<Link
+						href={linkItem.link}
+						className={`${styles.linkItem} ${
+							router.pathname === linkItem.link
+								? styles["linkItem--active"]
+								: ""
+						}`}
+						key={ind}
+					>
+						{linkItem.icon}
+						<p>{linkItem.title}</p>
+					</Link>
+				))}
 			</div>
-		</header>
+		</>
 	);
 };
 
